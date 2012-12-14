d643cf40715fafc4727b1e14348110c18e7a1224
mkandel <marc.kandel.junk@gmail.com>
2012-12-13 17:51:43 -0800

    Adding framework for parsing config files to Marc::Common

---
+++ lib/common/lib/Marc/Common/ConfigParser.pm
@@ -0,0 +1,37 @@
+package Marc::Common::ConfigParser;
+
+use warnings;
+use strict;
+
+use Data::Dumper;
+# Some Data::Dumper settings:
+local $Data::Dumper::Useqq  = 1;
+local $Data::Dumper::Indent = 3;
+
+local $| = 1;
+
+## We'll try Moose
+use Moose;
+use namespace::autoclean;
+with 'Marc::Common::Person';
+
+our $VERSION = '0.01';
+
+## class variable??!!??
+my $id = 0;
+
+## Moose attribute declarations:
+## required:
+has 'filename'          => (is => 'rw', isa => 'Str', required => 1 );
+has 'format'            => (is => 'rw', isa => 'Str', default  => 'yaml', required => 1, );
+has 'paths'             => (is => 'rw', isa => 'ArrayRef[Str]', default  => 'none', required => 1, );
+
+sub save {
+    my $self = shift;
+
+    return undef;
+}
+
+1; # End of Module
+
+__END__
