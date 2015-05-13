72f7955f5fa51461121a71f38bc2a4db249a770f
marc kandel <mkandel@sjc-mkandel-mba.arubanetworks.com>
2015-05-13 11:31:28 -0700

    Adding some POD

--- lib/common/lib/Marc/Common/Object.pm
+++ lib/common/lib/Marc/Common/Object.pm
@@ -1,5 +1,15 @@
 package Marc::Common::Object;
 
+=head1 NAME
+
+Marc::Common::Object
+
+=head1 VERSION
+
+Version 0.0.1
+
+=cut
+
 use warnings;
 use strict;
 
@@ -22,13 +32,59 @@ our $VERSION = '0.0.1';
 ## Derived classes MUST implement "require"d methods
 requires 'save';
 
-has 'debug'   => (is => 'rw', isa => 'Str', required => 1, default => 0 );
+=over
+
+=item debug()
+
+    FUNCTION: Enable/Disable debug output from an object
+
+   ARGUMENTS: 0/1
+
+     RETURNS: N/A
+
+=back
+
+=cut
+
+has 'debug'   => (is => 'rw', isa => 'Bool', required => 1, default => 0 );
+
+=over
+
+=item dump()
+
+    FUNCTION: Return a Data::Dumper dump of an object
+
+   ARGUMENTS: None
+
+     RETURNS: Data::Dumper dump of an object
+
+=back
+
+=cut
 
 sub dump {
     my $self = shift;
 
     return Dumper $self;
 }
+
+=over
+
+=item new()
+
+    FUNCTION: Constructor decorator
+
+   ARGUMENTS: N/A
+
+     RETURNS: Nothing, adds extra debug output
+
+       NOTES: If you pass 'debug => 1' to new()
+              the constructor will print a dump
+              of the newly created object.
+
+=back
+
+=cut
 
 after 'new' => sub { 
     my $class = shift;
@@ -41,11 +97,39 @@ after 'new' => sub {
     }
 };
 
+=over
+
+=item is_obj()
+
+    FUNCTION: Am I an object?
+
+   ARGUMENTS: None
+
+     RETURNS: 1 if an object, 0 if not (we'd be the class in that case)
+
+=back
+
+=cut
+
 sub is_obj {
     my $self = shift;
 
     return ref $self ? 1 : 0;
 }
+
+=over
+
+=item assert_is_obj()
+
+    FUNCTION: Asserts on 'Am I an object'
+
+   ARGUMENTS: None
+
+     RETURNS: Nothing, croak's if we're not an object
+
+=back
+
+=cut
 
 sub assert_is_obj {
     my $self = shift;
@@ -55,11 +139,40 @@ sub assert_is_obj {
     }
 }
 
+=over
+
+=item is_class()
+
+    FUNCTION: Am I a class?
+
+   ARGUMENTS: None
+
+     RETURNS: 1 if a class, 0 if not (we'd be an object in that case)
+
+=back
+
+=cut
+
 sub is_class {
     my $class = shift;
 
     return !$class->is_obj();
 }
+
+
+=over
+
+=item assert_is_class()
+
+    FUNCTION: Asserts on 'Am I a class'
+
+   ARGUMENTS: None
+
+     RETURNS: Nothing, croak's if we're not a class
+
+=back
+
+=cut
 
 sub assert_is_class {
     my $class = shift;
@@ -72,3 +185,4 @@ sub assert_is_class {
 1; # End of Module
 
 __END__
+
